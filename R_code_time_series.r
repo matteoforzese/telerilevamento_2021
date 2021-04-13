b3a5cf82946c01679b165673333036e2ff32a26d
matteoforzese <80624808+matteoforzese@users.noreply.github.com>
2021-04-13 18:14:09 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -3,6 +3,7 @@
 #Data and code from Emanuela Cosma
 
 library(raster)
+library(rasterVis)
 setwd("C:/lab/greenland")
 #creare oggetto Rasterlayer
 lst_2000<-raster("lst_2000.tif")
@@ -16,7 +17,6 @@ plot(lst_2005)
 plot(lst_2010)
 plot(lst_2015)
 #lista di file 
-list.files()
 r.list<-list.files(pattern="lst")
 r.list
 #funzione che applica un'altra funzione a una lista di file 
@@ -24,6 +24,27 @@ import<-lapply(r.list, raster)
 import
 #riunire file in un unico blocco
 TGr<-stack(import)
+TGr
 plot(TGr)
 #sovrapposizione tre immagini associate ad R, G e B
 plotRGB(TGr,1,2,3,stretch="Lin")
+#singola legenda per blocco intero che viene plottato tutto insieme
+levelplot(TGr)
+cl<-colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+levelplot(TGr, col.regions=cl,names.attr=c("July 2000", "July 2005","July 2010","July 2015"))
+levelplot(TGr, col.regions=cl, main="LST variation in time", names.attr=c("July 2000", "July 2005","July 2010","July 2015"))
+#Melt, dati sullo scioglimento
+#nuova lista
+meltlist<-list.files(pattern="melt")
+meltgreenland<-lapply(meltlist, raster)
+melt<-stack(meltgreenland)
+levelplot(melt)
+#sottrazione tra due matrici di dati
+melt_amount<-melt$X2007annual_melt-melt$X1979annual_melt
+clb<-colorRampPalette(c("white","blue","pink","red"))(100)
+plot(melt_amount,col=clb)
+
+install.packages("knitr")
+
+
