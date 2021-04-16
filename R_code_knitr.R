fa6d84b9eab5713de673314738368591fd1180de
matteoforzese <80624808+matteoforzese@users.noreply.github.com>
2021-04-16 10:56:31 +0200

    Update R_code_knitr.R

--- R_code_knitr.R
+++ R_code_knitr.R
@@ -1,3 +1,5 @@
 #using knitr to create a report
 setwd("C:/lab/")
 library(knitr)
+stitch("R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+#risolto errore aggiungendo .txt
