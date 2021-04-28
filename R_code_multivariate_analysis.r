f65cb88e7c125b58bd5278dfaa229b4926c0449b
matteoforzese <80624808+matteoforzese@users.noreply.github.com>
2021-04-28 12:30:53 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -7,3 +7,14 @@ plot(p224r63_2011)
 plot(p224r63_2011$B1_sre, p224r63_2011$B2_sre, col="red", pch=19, cex=2)
 #per trovare correlazione tra tutte le variabili a due a due
 pairs(p224r63_2011)
+#a causa di pesantezza di pca diminuiamo la grandezza del file abbassandone la risoluzione
+#aggregate cells: resampling (ricampionamento)
+p224r63_2011res<-aggregate(p224r63_2011, fact=10)
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin")
+#principal component analysis: passiamo asse lungo...?
+p224r63_2011res_pca <- rasterPCA(p224r63_2011res)
+summary(p224r63_2011res_pca$model)
+dev.off()
+plotRGB(p224r63_2011res_pca$map, r=1,g=2,b=3,stretch="lin")
