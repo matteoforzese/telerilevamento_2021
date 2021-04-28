8e95d044700e692a2095775cfc1440c91a6bb29a
matteoforzese <80624808+matteoforzese@users.noreply.github.com>
2021-04-28 12:57:47 +0200

    Create R_code_vegetation_indices.r

---
+++ R_code_vegetation_indices.r
@@ -0,0 +1,7 @@
+library(raster)
+setwd("C:/lab/")
+defor1 <- brick("defor1.png")
+defor2 <- brick("defor2.png")
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
