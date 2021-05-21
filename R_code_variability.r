9283ed68a3906b35fb5c88dd09685973e7695d64
matteoforzese <80624808+matteoforzese@users.noreply.github.com>
2021-05-21 10:51:34 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -1,5 +1,9 @@
 library(raster)
 library(RStoolbox)
+library(ggplot2) #for ggplot plotting
+library(gridExtra) #for plotting ggplots togheter
+install.packages("viridis") #pacchetto con scale di colore predefinite
+library (viridis)
 setwd("C:/lab/")
 sent <- brick("sentinel.png")
 sent
@@ -28,5 +32,41 @@ plot(sentpca$map)
 #per vedere da cosa è composta
 sentpca
 summary(sentpca$model)
-#la prima componente spiega più delle altre 
+#la prima componente spiega più delle altre perchè l'asse viene passato dentro il ... più ampio
+
+
+#guardiamo le componenti di sentpca, la prima si chiama PC1
+pc1<-sentpca$map$PC1
+focalpc1_sd5<-focal(pc1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+plot(focalpc1, col=clsd)
+
+#funzione source carica direttamente un file da una cartella del pc
+source("source_test_lezione.r")
+source("source_ggplot.r")
+
+#aprire una nuova finestra con ggplot
+ggplot()+
+#determina "geometria" del raster
+geom_raster(focalpc1_sd5, mapping=aes(x=x, y=y, fill=layer))
+#creare colorramppalette predefinita direttamente tramite viridis
+p1<-ggplot() +
+geom_raster(focalpc1_sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis()
+#utilizzo colorramppalette "magma"
+p2<-ggplot() +
+geom_raster(focalpc1_sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option = "magma")  +
+ggtitle("Standard deviation of PC1 by magma colour scale")
+#utilizzo colorramppalette "inferno"
+p3<-ggplot() +
+geom_raster(focalpc1_sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option = "inferno")  +
+ggtitle("Standard deviation of PC1 by inferno colour scale")
+#utilizzare un grid arrange diverso 
+#in verticale
+grid.arrange(p1,p2,p3, nrow=3)
+#in orizzontale
+grid.arrange(p1,p2,p3, nrow=1)
+
+
 
