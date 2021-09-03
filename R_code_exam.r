3cc0aa53b1c10070b03a5ee2ae7616574ce08246
matteoforzese <80624808+matteoforzese@users.noreply.github.com>
2021-09-03 18:25:12 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1,1 +1,31 @@
+setwd("C:/lab/ESAME")
+library(raster)  #permette l'utilizzo dei raster e funzioni annesse
+library(RStoolbox) #permette l'uso della Unsupervised Classification
+library(ggplot2)  #permette l'uso delle funzioni ggplot
+library(gridExtra)   #permette l'uso e creazione di griglie, tabelle e grafici
+
+H1982<-brick("STHE82.jpg")
+H1986<-brick("STHE86.jpg")
+H1990<-brick("STHE90.jpg")
+H1993<-brick("STHE93.jpg")
+H1998<-brick("STHE98.jpg")
+H2002<-brick("STHE02.jpg")
+H2006<-brick("STHE06.jpg")
+H2010<-brick("STHE10.jpg")
+H2015<-brick("STHE15.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
