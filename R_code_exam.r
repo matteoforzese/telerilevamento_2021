bc09b45d560040a1e33a3fe8bb510bcea9c40f87
matteoforzese <80624808+matteoforzese@users.noreply.github.com>
2021-09-05 12:54:23 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -3,6 +3,7 @@ library(raster)  #permette l'utilizzo dei raster e funzioni annesse
 library(RStoolbox) #permette l'uso della Unsupervised Classification
 library(ggplot2)  #permette l'uso delle funzioni ggplot
 library(gridExtra)   #permette l'uso e creazione di griglie, tabelle e grafici
+library(rasterdiv)
 
 H1979<-brick("STHE79.jpg")
 H1980<-brick("STHE80.jpg")
@@ -16,16 +17,6 @@ H2006<-brick("STHE06.jpg")
 H2010<-brick("STHE10.jpg")
 H2015<-brick("STHE15.jpg")
 
-H1979<-reclassify(H1979, cbind(253:255, NA))
-H1980<-reclassify(H1980, cbind(253:255, NA))
-H1982<-reclassify(H1982, cbind(253:255, NA))
-H1986<-reclassify(H1986, cbind(253:255, NA))
-
-
-plotRGB(H1986, r=1, g=2, b=3)
-par(mfrow=c(2,1))
-plot(H1980$map)
-plotRGB(IH1986$map)
 
 BWG<-colorRampPalette(c("black","white","green"))(100)
 
@@ -39,7 +30,6 @@ MSH1998 <- unsuperClass(H1998, nClasses=3,stretch="lin")
 MSH2002 <- unsuperClass(H2002, nClasses=3,stretch="lin")
 MSH2006 <- unsuperClass(H2006, nClasses=3,stretch="lin")
 
-
 par(mfrow=c(5,2))
 plot(MSH1979$map, col=BWG)
 plot(MSH1980$map, col=BWG)
@@ -51,7 +41,8 @@ plot(MSH1998$map, col=BWG)
 plot(MSH2002$map, col=BWG)
 plot(MSH2006$map, col=BWG)
 
-
+plot(MSH1979$map)
+freq(MSH1979$map)
 
 
 BWG<-colorRampPalette(c("black","white","green"))(100)
@@ -64,9 +55,12 @@ plot(MSH1986$map, col=BWG)
 plotRGB(H1982, r=2, g=2, b=1)
 
 
+H1979<-reclassify(H1979, cbind(253:255, NA))
+H1980<-reclassify(H1980, cbind(253:255, NA))
+H1982<-reclassify(H1982, cbind(253:255, NA))
+H1986<-reclassify(H1986, cbind(253:255, NA))
 
-
-
+MSH1979 <- unsuperClass(H1979$MSH1979.2, nClasses=3,stretch="lin")
 
 
 
